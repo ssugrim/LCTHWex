3af22720db0f5a31357e86bd0a1432211e021aa2
Shridatt Sugrim <shridatt.sugrim@gmail.com>
2016-10-01 22:37:00 -0400

    An example that uses shared mem as a flag

--- thread/ex_thread.c
+++ thread/ex_thread.c
@@ -7,17 +7,34 @@
 typedef struct{
     int id;
     int count;
+    int *p;
 } arg_t;
 
 void * worker (void * func_arg){
     check(func_arg != NULL,"Bad arg");
     int limit = ((arg_t *) func_arg)->count;
     int id = ((arg_t *) func_arg)->id;
+    int *flag = ((arg_t *) func_arg)->p;
     int i = 0;
 
-    for (i=0; i < limit; i++){
-        debug("Thread %d at %d",id,limit);
-        sleep(1);
+    if(id == 1){
+        for (i=0; i < limit; i++){
+            debug("Thread %d at %d",id,i);
+            sleep(0.5);
+        }
+        *flag = 1;
+    }
+    else{
+        while(1){
+            if (*flag == 1){
+                return NULL;
+            }
+            else{
+                i++;
+                debug("Thread %d at %d",id,i);
+                sleep(0.25);
+            }
+        }
     }
 
     return NULL;
@@ -34,17 +51,27 @@ int main(int argc, char * argv[]){
 
     arg_1 = malloc(sizeof(arg_t));
     arg_2 = malloc(sizeof(arg_t));
+    int *flag = malloc(sizeof(int));
+    *flag = 0;
+
     arg_1->id = 1;
     arg_1->count = 100;
+    arg_1->p = flag;
 
     arg_2->id = 2;
     arg_2->count = 10;
+    arg_2->p = flag;
 
-    pthread_create(&t_1, NULL, worker, arg_1);
+    int result_code = 0;
+    result_code = pthread_create(&t_1, NULL, worker, arg_1);
+    check(result_code == 0, "problem creating thread 1");
     pthread_create(&t_2, NULL, worker, arg_2);
+    check(result_code == 0, "problem creating thread 2");
 
-    pthread_join(t_1, NULL);
     pthread_join(t_2, NULL);
 
     return 0;
+
+error:
+    return 1;
 }
